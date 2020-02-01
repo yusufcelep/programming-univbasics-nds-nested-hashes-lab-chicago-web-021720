c71834670c3d4d0f1cbd6f78015ab5071fd31506
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-01 03:08:11 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,20 +1,21 @@
-# Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
-
-DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G = { name:  "Kate Gompert", occupation: "None" }
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+DON_G = {name: "Don Gately", occupation: "Live-in Staff" }
+JOELLE_VD = {name: "Joelle van Dyne", occupation: "Radio Personality" }
+PAT_M =  {name: "Pat Monteseian", occupation: "Staff" }
+KATE_G = {name: "Kate Gompert", occupation: "None" }
+BRUCE_G = {name: "Bruce Green", occupation: "Fan of Mildred"
 
 def assembled_aoh
-  # Build an array that contains (or, "nests") the constants into a single
-  # Array. Ruby constants are denoted by ALL_CAPS
-  array = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  array = [
+    DON_G, 
+    JOELLE_VD, 
+    PAT_M, 
+    KATE_G, 
+    BRUCE_G
+    ]
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+  array = [{:name = }]
 end
 
 def aoh_lookup(aoh, row, key)
